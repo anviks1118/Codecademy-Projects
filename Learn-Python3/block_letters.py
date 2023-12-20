77770fc08e415664c39c1c8547bbde04b5c2efec
anviks1118 <anviks1118@gmail.com>
2023-12-19 17:33:21 -0800

    Added Block Letters

---
+++ Learn-Python3/block_letters.py
@@ -0,0 +1,14 @@
+"""
+Goal: ASCII Art
+"""
+
+print("    A       K     K")
+print("  A   A     K    K")
+print(" A     A    K   K")
+print(" A A A A    K K")
+print(" A     A    K   K")
+print(" A     A    K    K")
+print(" A     A    K     K")
+
+#Anvita Kisara
+#I started dancing when I was three years old
